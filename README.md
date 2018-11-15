9651bad70430341c54ccba4e123ed54193970653
Anna Kanska <ann.kanska@gmail.com>
2018-11-15 13:26:06 +0100

    bootstrap modal added to CatView

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 Welcome to the most amazing, spectacular and interstellar adoption web in the whole galaxy! In this web you will be able to adopt an Space Cat (normal cats not allowed) based in your personality.
 10.. 9.. 8.. 7.. 6.. 5.. 4.. 3.. 2.. 1.. Let's go!
 
-
 ## Setup
+
 Run `yarn` to install packages.
 Run `yarn start` to run the development server.
+Run `yarn development`
 
 ### Database Prep
+
 Type `createdb spacecats` to create a database in PostgreSQL.
 Run `node server/models/database.js` to create the table structure.
 Run `node server/models/data.js` to fill the tables.
@@ -16,15 +18,18 @@ Run `node server/models/data.js` to fill the tables.
 ## Basic Requirements
 
 ### Home page
-  - Find out what cat planet you are!
-  - Click on a planet to start the test.
-  - Show three planets.
+
+- Find out what cat planet you are!
+- Click on a planet to start the test.
+- Show three planets.
 
 ### Test page
 
 ### Portfolio result page
-  - Shows all the breeds which match with the results.
-  - On click on an image it will display the details and the possibility to adopt her/him.
+
+- Shows all the breeds which match with the results.
+- On click on an image it will display the details and the possibility to adopt her/him.
 
 ## Notes
+
 _This is a student project that was created at [Codely](http://codely.tech), a full stack development bootcamp in Barcelona._
--- src/components/catsView.js
+++ src/components/catsView.js
@@ -1,43 +1,92 @@
 import React from "react";
 //import styles from './styles.css';
 
-class CatsView extends React.Component { 
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: false, 
-        cats: {}
-      }
-    }; 
+class CatsView extends React.Component {
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-      fetch(`http://localhost:3000/api/v1/spacecats/results?id=${this.props.userId}`)
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+
+    this.state = {
+      error: false,
+      cats: {},
+      featured: 0,
+      show: false
+    };
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleHide() {
+    this.setState({ show: false });
+  }
+
+  componentDidMount() {
+    fetch(
+      `http://localhost:3000/api/v1/spacecats/results?id=${this.props.userId}`
+    )
       .then(res => res.json())
       .then(json => {
         this.setState({
           cats: json
-        })
+        });
       })
-      .catch(error => { 
+      .catch(error => {
         this.setState({
           error: true
-        })        
-      })
+        });
+      });
+  }
+
+  addFeatured(i) {
+    this.setState({
+      featured: i
+    });
+  }
+
+  render() {
+    let gats = this.state.cats;
+    let catPhotos = [];
+    for (let key in gats) {
+      catPhotos.push(
+        <img
+          key={key}
+          src={gats[key].media.photos.photo[3].$t}
+          onClick={this.handleShow()}
+        />
+      );
     }
 
-    render() {
-
-      let gats = this.state.cats;
-      let catPhotos = [];
-      for(let key in gats) {
-      catPhotos.push(<img key={key} src={gats[key].media.photos.photo[3].$t} />) 
-      }
-    
-      return(
+    return (
+      <div>
         <div>{catPhotos}</div>
-      )
-    }
-
-}    
+        <div>
+          <Modal
+            {...this.props}
+            show={this.state.show}
+            onHide={this.handleHide}
+            dialogClassName="custom-modal"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-lg">
+                Modal heading
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img />
+              <p>modal here</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default CatsView;
