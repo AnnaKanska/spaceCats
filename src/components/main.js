7a176ade0566028b8fb71fd5e99db39f6fcf554e
Anna Kanska <ann.kanska@gmail.com>
2018-11-19 12:56:14 +0100

    styling changes

--- src/components/main.js
+++ src/components/main.js
@@ -1,40 +1,55 @@
 import React from "react";
 import TestView from "./testView";
 
-
-class Main extends React.Component { //class Main inherits from react
+class Main extends React.Component {
+  //class Main inherits from react
   constructor(props) {
     super(props); //inherits properties
-    this.state = {test: false}
-  }; 
+    this.state = { test: false };
+  }
 
   goTest() {
-      this.setState({test: true});
+    this.setState({ test: true });
   }
 
   render() {
-
     let home;
 
     if (!this.state.test) {
-        home =( <div>
-            <h1>Space Cats</h1>
-            <h3>Find out what cat planet you are!</h3>
-            <img src="../src/assets/planet1.png" onClick={() => this.goTest()}/>
-            <img src="../src/assets/planet2.png" onClick={() => this.goTest()}/>
-            <img src="../src/assets/planet3.png" onClick={() => this.goTest()}/>
-        </div>)
+      home = (
+        <div>
+          <div id="title">
+            <img src="../src/assets/cat1.png" className="small-cat" />
+            <h1 id="titlecats">Space Cats</h1>
+            <img
+              src="../src/assets/cat2.png"
+              className="small-cat"
+              id="smaller"
+            />
+          </div>
+
+          <h3>
+            Welcome to the interstellar, most amazing and spectacular cat
+            adoption service in the whole galaxy!
+            <br />
+            Here you will be able to adopt an Space Cat based in your
+            personality.
+            <br />
+            Click on the planet to start your personality inquiry!
+          </h3>
+          <div id="turning_planet">
+            <img
+              src="../src/assets/turning_planet.gif"
+              onClick={() => this.goTest()}
+            />
+          </div>
+        </div>
+      );
     } else {
-        home = <TestView />
+      home = <TestView />;
     }
-    
-    return (
-      <div>
-        {home}
-      </div>
-    );
 
-
+    return <div>{home}</div>;
   }
 }
 
--- src/components/testView.js
+++ src/components/testView.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import CatsView from "./catsView";
-
 
 class TestView extends React.Component {
   constructor(props) {
-    super(props); 
+    super(props);
     //this states will be stored in the users table
     this.state = {
       q1: "T",
@@ -13,102 +12,142 @@ class TestView extends React.Component {
       q4: "T",
       email: "",
       showCats: false
-    }
-  }; 
+    };
+  }
 
-  handleChange (event) {
+  handleChange(event) {
     const name = event.target.name;
 
     this.setState({
       [name]: event.target.value
-    })
+    });
   }
 
-  
-  submitTest(event){
+  submitTest(event) {
     event.preventDefault();
     let results = this.state.q1 + this.state.q2 + this.state.q3 + this.state.q4;
 
     fetch("http://localhost:3000/api/v1/spacecats", {
-      method: 'POST',
-      body: JSON.stringify({'email': this.state.email, 'results': results}),
+      method: "POST",
+      body: JSON.stringify({ email: this.state.email, results: results }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
-    }).then(res => res.json()) 
-    .then(json => { 
-      this.setState({
-        showCats: true,
-        id: json.body[0].id
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          showCats: true,
+          id: json.body[0].id
+        });
       })
-    }) 
-    .catch(error => {
-      this.setState({
-        error: true
-      })
-    })       
+      .catch(error => {
+        this.setState({
+          error: true
+        });
+      });
   }
-
 
   render() {
-  
     let view;
-    
+
     if (!this.state.showCats) {
-      view =( 
-      <div>
-        <h1>Test</h1>
+      view = (
+        <div>
+          <h1 id="test_title">Test</h1>
+          <div id="test_q">
+            <p>What type of relationship am I looking for?</p>
+            <br />
+            <form>
+              <select
+                name="q1"
+                value={this.state.q1}
+                onChange={e => this.handleChange(e)}
+              >
+                <option value="T">
+                  Sometimes I need some company but sometimes I like beeing on
+                  my own.
+                </option>
+                <option value="F">
+                  I prefer sickness relationships, why going to the bathroom
+                  alone if I can have company?
+                </option>
+              </select>
+            </form>
 
-        <p>What type of relationship am I looking for?</p>
-        <form>
-          <select name="q1" value={this.state.q1} onChange={(e) => this.handleChange(e)}>
-            <option value="T">Sometimes I need some company but sometimes I like beeing on my own.</option>
-            <option value="F">I prefer sickness relationships, why going to the bathroom alone if I can have company?</option>
-          </select>
-        </form>
+            <p>
+              I need action, so my parthner must have a lot of energy <br /> to
+              spend with me.
+            </p>
+            <br />
+            <form>
+              <select
+                name="q2"
+                value={this.state.q2}
+                onChange={e => this.handleChange(e)}
+              >
+                <option value="T">Of course! I can spend hours playing!</option>
+                <option value="F">
+                  Not really, I prefer some company while lying on my couch.
+                </option>
+              </select>
+            </form>
 
-            <p>I need action, so my parthner must have a lot of energy to spend with me.</p>
-        <form>
-          <select name="q2" value={this.state.q2} onChange={(e) => this.handleChange(e)}>
-            <option value="T">Of course! I can spend hours playing!</option>
-            <option value="F">Not really, I prefer some company while lying on my couch.</option>
-          </select>
-        </form>
-
-            <p>I am super socible, so I expect my parthner to like having visits.</p>
-        <form>
-          <select name="q3" value={this.state.q3} onChange={(e) => this.handleChange(e)}>
-            <option value="T">Absolutely!</option>
-            <option value="F">Meh.. I am more a lonely person.</option>
-          </select>
-        </form>
+            <p>
+              I am super socible, so I expect my parthner to like having visits.
+            </p>
+            <br />
+            <form>
+              <select
+                name="q3"
+                value={this.state.q3}
+                onChange={e => this.handleChange(e)}
+              >
+                <option value="T">Absolutely!</option>
+                <option value="F">Meh.. I am more a lonely person.</option>
+              </select>
+            </form>
 
             <p>I like romance, but I prefer not beeing kissed all the time.</p>
-        <form>
-          <select name="q4" value={this.state.q4} onChange={(e) => this.handleChange(e)}>
-            <option value="T">No! I am a love machine and I need love all day long!</option>
-            <option value="F">Exactly, I like kisses, but I need my own space.</option>
-          </select>
-        </form>
-      
-        <br></br>
-
-        <input type="text" name="email" onChange={(e) => this.handleChange(e)}></input>    
-        <button onClick={(e)=>this.submitTest(e)} >SUBMIT</button>               
-      </div>
-      )
+            <form>
+              <br />
+              <select
+                name="q4"
+                value={this.state.q4}
+                onChange={e => this.handleChange(e)}
+              >
+                <option value="T">
+                  No! I am a love machine and I need love all day long!
+                </option>
+                <option value="F">
+                  Exactly, I like kisses, but I need my own space.
+                </option>
+              </select>
+            </form>
+          </div>
+          <br />
+          <div id="submit">
+            <input
+              type="text"
+              name="email"
+              onChange={e => this.handleChange(e)}
+              placeholder="my email"
+            />
+            <br />
+            <img
+              src="../src/assets/rocket.png"
+              id="submit_button"
+              onClick={e => this.submitTest(e)}
+            />
+          </div>
+        </div>
+      );
     } else {
-      view = <CatsView  userId={this.state.id}/>
+      view = <CatsView userId={this.state.id} />;
     }
 
-    return (
-      <div>
-        {view}
-      </div>
-    );
+    return <div>{view}</div>;
   }
 }
-
-
 
 export default TestView;
